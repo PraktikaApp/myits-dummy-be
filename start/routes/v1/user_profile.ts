f129ad62232415a69537bf73a7a70461662523b3
Agus Darmawan <5024211070@mhs.its.ac.id>
2024-11-01 06:35:01 +0700

    feat: final

--- start/routes/v1/user_profile.ts
+++ start/routes/v1/user_profile.ts
@@ -1,19 +1,19 @@
-const CoursesController = () => import('#controllers/courses_controller')
+const UserProfilesController = () => import('#controllers/user_profiles_controller');
 import { middleware } from '#start/kernel'
 import router from '@adonisjs/core/services/router'
 
 export default function courseRoute() {
   router
     .group(() => {
-      router.get('/', [CoursesController, 'index'])
+      router.get('/', [UserProfilesController, 'index'])
       router
         .group(() => {
-          router.post('/', [CoursesController, 'store'])
-          router.get('/:id', [CoursesController, 'show'])
-          router.patch('/:id', [CoursesController, 'update'])
-          router.delete('/:id', [CoursesController, 'destroy'])
+          router.post('/', [UserProfilesController, 'store'])
+          router.get('/:id', [UserProfilesController, 'show'])
+          router.patch('/:id', [UserProfilesController, 'update'])
+          router.delete('/:id', [UserProfilesController, 'destroy'])
         })
         .middleware(middleware.auth({ guards: ['api'] }))
     })
-    .prefix('/courses')
+    .prefix('/user_profile')
 }
